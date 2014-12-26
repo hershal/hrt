fbaa007a9571f0f7dcff4dbf0375ec791c6406b2
Hershal Bhave <hershal.bhave@gmail.com>
2014-12-25 20:25:44 -0600

    added custom vector class

---
+++ src/core/vector.cpp
@@ -0,0 +1,104 @@
+/*! Vector implementation for Hershal Bhave's hrt renderer */
+
+#include "vector.hpp"
+
+vector::hasnan() const -> bool {
+    if (isnan(x) || isnan(y) || isnan(z) || ) {
+        return true;
+    }
+}
+
+auto vector::operator-() const -> vector {
+    return (vector(-this.x,
+                   -this.y,
+                   -this.z));
+}
+
+auto vector::operator+(const vector &v) const -> vector {
+    return (vector(this.x + v.x,
+                   this.y + v.y,
+                   this.z + v.z));
+}
+
+auto vector::operator+=(const vector &v) -> vector& {
+    this.x += v.x;
+    this.y += v.y;
+    this.z += v.z;
+    return *this;
+}
+
+auto vector::operator-(const vector &v) const -> vector {
+    return (vector(this.x - v.x,
+                   this.y - v.y,
+                   this.z - v.z));
+}
+
+auto vector::operator-=(const vector &v) -> vector& {
+    this.x -= v.x;
+    this.y -= v.y;
+    this.z -= v.z;
+    return *this;
+}
+
+auto vector::operator*(float f) const -> vector {
+    return (vector(this.x * f,
+                   this.y * f,
+                   this.z * f));
+}
+
+auto vector::operator*=(float f) -> vector& {
+    this.x *= f;
+    this.y *= f;
+    this.z *= f;
+    return *this;
+}
+
+auto vector::operator/(float f) const -> vector {
+
+    if(FLAlmostEqualRelative(f, 0)) {
+        printf("SEVERE: vector: Tried to scale-by-reciprocal with a "
+               "Zero: f=%f, x=%f, y=%f, z=%f ", f, x, y, z);
+    }
+
+    float inverse = 1.0f/f;
+
+    return (vector(this.x * inverse,
+                   this.y * inverse,
+                   this.z * inverse));
+}
+
+auto vector::operator/=(float f) -> vector& {
+    if(FLAlmostEqualRelative(f, 0)) {
+        printf("SEVERE: vector: Tried to scale-assign-by-reciprocal with a "
+               "Zero: f=%f, x=%f, y=%f, z=%f ", f, x, y, z);
+    }
+
+    float inverse = 1.0f/f;
+
+    this.x *= inverse;
+    this.y *= inverse;
+    this.z *= inverse;
+    return *this;
+}
+
+auto vector::operator[] (const std::size_t i) const -> const float {
+    if(i >= NUM_DIMENSIONS) {
+        printf("SEVERE: vector: tried to get vector component greater"
+               "than the number of dimensions (%i)", NUM_DIMENSIONS);
+    }
+
+    /* NOT GUARANTEED PORTABLE! */
+    /* TODO: Test this */
+    return (&x)[i];
+}
+
+auto vector::operator[] (const std::size_t i) -> float& {
+    if(i >= NUM_DIMENSIONS) {
+        printf("SEVERE: vector: tried to set vector component greater"
+               "than the number of dimensions (%i)", NUM_DIMENSIONS);
+    }
+
+    /* NOT GUARANTEED PORTABLE! */
+    /* TODO: Test this */
+    return (&x)[i];
+}
---
+++ src/core/vector.hpp
@@ -0,0 +1,64 @@
+/*! Vector header for Hershal Bhave's hrt renderer */
+
+/* /class vector */
+/* DO NOT use the std namespace as this name will conflict with that
+   namespace */
+
+#include <cmath>
+#include <cstdio>
+
+#define NUM_DIMENSIONS 3
+
+class vector {
+
+public:
+
+    vector() { this.x = 0; this.y = 0; this.z = 0; }
+
+    vector(float x, float y, float z) {
+        this.x = x; this.y = y; this.z = z;
+        if (hasnan()) {
+            printf("SEVERE: vector: Tried to construct a vector with a "
+                   "NaN: x=%f, y=%f, z=%f ", x, y, z);
+        }
+    }
+
+    /*! \brief Returns if the vector has at least one not-a-number
+        element. */
+    auto hasnan() const -> bool;
+
+    /*! \brief vector unary negation operator. */
+    auto operator-() const -> vector;
+
+    /*! \brief vector addition operator. */
+    auto operator+(const vector &v) const -> vector;
+
+    /*! \brief vector accumulate operator. */
+    auto operator+=(const vector &v) -> vector&;
+
+    /*! \brief vector subtraction operator. */
+    auto operator-(const vector &v) const -> vector;
+
+    /*! \brief vector reduce operator. */
+    auto operator-=(const vector &v) -> vector&;
+
+    /*! \brief vector scale operator. */
+    auto operator*(const vector &v) const -> vector;
+
+    /*! \brief vector scale-assign operator. */
+    auto operator*=(const vector &v) -> vector&;
+
+    /*! \brief vector scale-by-reciprocal operator. */
+    auto operator*(const vector &v) const -> vector;
+
+    /*! \brief vector scale-assign-by-reciprocal operator. */
+    auto operator*=(const vector &v) -> vector&;
+
+    /*! \brief vector get-value operator. */
+    auto operator[] (const std::size_t i) const -> const float;
+
+    /*! \brief vector set-value operator. */
+    auto operator[] (const std::size_t i) -> float&;
+    
+    float x, y, z;
+};

d98495fc3680e0aa7c8f0e01431597441cedb007
Hershal Bhave <hershal.bhave@gmail.com>
2015-07-05 21:05:03 -0500

    transform: refilled

--- lib/core/transform.hpp
+++ lib/core/transform.hpp
@@ -29,7 +29,7 @@ namespace hrt {
                 hrt::matrix4x4::copy(m_inv, mat_inv);
             }
 
-            #include "common_matrices.hpp"
+#include "common_matrices.hpp"
 
             auto swaps_handedness(void) const -> bool;
 
@@ -37,18 +37,18 @@ namespace hrt {
 
             auto rotate(float rads, const vector *axis) -> transform;
 
-          auto operator()(const point &p) const -> point;
-          auto operator()(const point &pt, point *p) const -> void;
+            auto operator()(const point &p) const -> point;
+            auto operator()(const point &pt, point *p) const -> void;
 
-          auto operator()(const vector &vec) const -> vector;
-          auto operator()(const vector &vec, vector *v) const -> void;
+            auto operator()(const vector &vec) const -> vector;
+            auto operator()(const vector &vec, vector *v) const -> void;
 
-          auto operator()(const ray &r) const -> ray;
-          auto operator()(const ray &r, ray *rr) const -> void;
+            auto operator()(const ray &r) const -> ray;
+            auto operator()(const ray &r, ray *rr) const -> void;
 
-          auto operator()(const bounding_box &b) const -> bounding_box;
+            auto operator()(const bounding_box &b) const -> bounding_box;
 
-          auto operator*(const transform &t) const -> transform;
+            auto operator*(const transform &t) const -> transform;
 
         private:
 
@@ -56,14 +56,14 @@ namespace hrt {
 
               Keep this in row-major order, i.e. mat[i][j] corresponds
               to $mat_{i,j}$.
-             */
+            */
             float mat[4][4];
 
             /*! \brief The inverted transformation matrix.
 
               Keep this in row-major order, i.e. mat[i][j] corresponds
               to $mat_{i,j}$.
-             */
+            */
             float mat_inv[4][4];
         };
     }

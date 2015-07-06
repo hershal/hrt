667219bdda3a9e9f061253bc414f5d31870f7799
Hershal Bhave <hershal.bhave@gmail.com>
2015-07-05 21:18:47 -0500

    anxiety: checkpoint

--- prototyping/anxiety.cpp
+++ prototyping/anxiety.cpp
@@ -1,6 +1,7 @@
 #include "../lib/core/ray.hpp"
 #include "../lib/core/vector.hpp"
 #include "../lib/core/transform.hpp"
+#include "../lib/utility/utility_functions.hpp"
 
 #include "GraphicsMagick/Magick++.h"
 
@@ -10,16 +11,18 @@ int main() {
 
     using namespace hrt::core;
 
-    /* constants */
-    /* resolution */
+    /* user-adjustable constants */
     size_t xres = 100;
     size_t yres = 100;
-
-    /* bounces */
     size_t numbounces = 3;
+    float fov_degrees = 75.f;
+    /* TODO: determine how the camera optics work */
+    float camera_radius = 5.f;
 
-    /* box enclosing the entire thing with extrema at 0 and 25 */
-    /* this box is fully diffusive */
+    /* computed constants */
+    float fov = fov_degrees/180*M_PI;
+    float fovmax = -fov/2;
+    float fovmin = fov/2;
 
     /* sphere */
     point sphere_origin(10.f, 5.f, 10.f);
@@ -28,18 +31,19 @@ int main() {
     /* camera */
     point camera_origin(10.f, 10.f, 0.f);
     vector camera_dir = sphere_origin - camera_origin;
-    float camera_fov = 90/180*M_PI;
 
     /* light */
     point light_origin(10.f, 20.f, 10.f);
     vector light_dir = sphere_origin - light_origin;
 
+    /* box enclosing the entire thing with extrema at 0 and 25 */
+    /* this box is fully diffusive */
+
     for (size_t i=0; i<xres; ++i) {
         for (size_t j=0; j<yres; ++j) {
             for (size_t k=0; k<numbounces; ++k) {
 
-                /* compute degrees */
-                /* ray r(camera_origin, ) */
+                /* compute angle into the scene */
 
             }
         }
